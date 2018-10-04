f513065296b3d7b04d370ffa6fd2af6742404acf
chidi onuekwusi <chidionuekwusi@gmail.com>
2018-10-04 21:26:02 +0100

    Updates

---
+++ precache-manifest.2b508628e0d081f3e4a4251cfa502597.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "965c7a5bba4c29059c598966fbec5ea2",
+    "url": "/Twaija-web/static/media/logo_.965c7a5b.png"
+  },
+  {
+    "revision": "15f5daad389433d22dd1",
+    "url": "/Twaija-web/static/css/main.1407966f.chunk.css"
+  },
+  {
+    "revision": "bed89ffe4cc7a2818f1b",
+    "url": "/Twaija-web/static/js/1.bed89ffe.chunk.js"
+  },
+  {
+    "revision": "ac4b3bc54d18b6b9a6e0",
+    "url": "/Twaija-web/static/js/runtime~main.ac4b3bc5.js"
+  },
+  {
+    "revision": "1d3c20a32dd0a9fa58f7140e158a7471",
+    "url": "/Twaija-web/static/media/twaija_1.1d3c20a3.png"
+  },
+  {
+    "revision": "15f5daad389433d22dd1",
+    "url": "/Twaija-web/static/js/main.15f5daad.chunk.js"
+  },
+  {
+    "revision": "9ac279f97fd30bde7b19709196d0a23c",
+    "url": "/Twaija-web/static/media/github.9ac279f9.png"
+  },
+  {
+    "revision": "e8ae5bbd72133b01afac677155d62967",
+    "url": "/Twaija-web/static/media/team.e8ae5bbd.jpg"
+  },
+  {
+    "revision": "a1b05efa8679720eab851334cc7f20e4",
+    "url": "/Twaija-web/static/media/twaija_2.a1b05efa.png"
+  },
+  {
+    "revision": "28e8c5af2d83c0b07144b1548f048b3f",
+    "url": "/Twaija-web/static/media/twaija_3.28e8c5af.png"
+  },
+  {
+    "revision": "4501f6b8960acbd11ab40bd8139f7fe2",
+    "url": "/Twaija-web/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/Twaija-web/precache-manifest.2b508628e0d081f3e4a4251cfa502597.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Twaija-web/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
